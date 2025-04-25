79ecbb6fc23059550346d0fd8df30fb22163ca33
Gianluigi De Marco <dem.gianluigi@gmail.com>
2025-04-25 21:51:50 +0200

    Update database user name and remove unused JPA properties
    Renamed database user from 'fido2-poc-id-be23' to 'fido2-poc' and updated corresponding grants in the SQL script. Removed unnecessary JPA properties from application.yaml to clean up redundant configuration.

--- script/DB-USER.sql
+++ script/DB-USER.sql
@@ -5,15 +5,15 @@ GO
 
 BEGIN TRANSACTION;
 
-CREATE USER [fido2-poc-id-be23] FROM EXTERNAL PROVIDER;
+CREATE USER [fido2-poc] FROM EXTERNAL PROVIDER;
 GO
 
-GRANT SELECT ON SCHEMA ::[fido-poc] TO [fido2-poc-id-be23];
-GRANT INSERT ON SCHEMA ::[fido-poc] TO [fido2-poc-id-be23];
-GRANT UPDATE ON SCHEMA ::[fido-poc] TO [fido2-poc-id-be23];
-GRANT DELETE ON SCHEMA::[fido-poc] TO [fido2-poc-id-be23];
-GRANT ALTER ON SCHEMA::[fido-poc] TO [fido2-poc-id-be23];
-GRANT CONTROL ON SCHEMA::[fido-poc] TO [fido2-poc-id-be23];
+GRANT SELECT ON SCHEMA ::[fido-poc] TO [fido2-poc];
+GRANT INSERT ON SCHEMA ::[fido-poc] TO [fido2-poc];
+GRANT UPDATE ON SCHEMA ::[fido-poc] TO [fido2-poc];
+GRANT DELETE ON SCHEMA::[fido-poc] TO [fido2-poc];
+GRANT ALTER ON SCHEMA::[fido-poc] TO [fido2-poc];
+GRANT CONTROL ON SCHEMA::[fido-poc] TO [fido2-poc];
 GO
 
 COMMIT
